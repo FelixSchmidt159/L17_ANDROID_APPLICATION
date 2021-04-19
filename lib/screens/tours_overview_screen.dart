0cbc4cdb02cdd725d2fc2c65695ae9391c2f2bf8
FelixSchmidt159 <flx159@yahoo.at>
2021-04-19 18:05:08 +0200

    First Page Layout

--- lib/screens/tours_overview_screen.dart
+++ lib/screens/tours_overview_screen.dart
@@ -12,11 +12,17 @@ class ToursOverviewScreen extends StatefulWidget {
 
 class _ToursOverviewScreenState extends State<ToursOverviewScreen> {
   List<Tour> _items;
-
+  int _selectedIndex = 0;
   @override
   void initState() {
     // _items = Provider.of<Tours>(context).items;
     super.initState();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
@@ -27,67 +33,114 @@ class _ToursOverviewScreenState extends State<ToursOverviewScreen> {
         title: Text('Fahrtenbuch'),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, position) {
-          return Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 6.0),
-                        child: Text(
-                          _items[position].licensePlate,
-                          style: TextStyle(
-                              fontSize: 22.0, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 12.0),
-                        child: Text(
-                          _items[position].roadCondition,
-                          style: TextStyle(fontSize: 18.0),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.grey.shade200,
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('hii'),
+                Text('hii'),
+                Text('hii'),
+              ],
+            ),
+          ),
+          Container(
+            height: 400,
+            child: ListView.builder(
+              itemBuilder: (context, position) {
+                return Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Text(
-                          "5m",
-                          style: TextStyle(color: Colors.grey),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  12.0, 12.0, 12.0, 6.0),
+                              child: Text(
+                                _items[position].licensePlate,
+                                style: TextStyle(
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  12.0, 6.0, 12.0, 12.0),
+                              child: Text(
+                                _items[position].roadCondition,
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                            ),
+                          ],
                         ),
                         Padding(
                           padding: const EdgeInsets.all(8.0),
-                          child: Icon(
-                            Icons.star_border,
-                            size: 35.0,
-                            color: Colors.grey,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Text(
+                                "5m",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Icon(
+                                  Icons.star_border,
+                                  size: 35.0,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-              Divider(
-                height: 2.0,
-                color: Colors.grey,
-              )
-            ],
-          );
-        },
-        itemCount: _items.length,
+                    Divider(
+                      height: 2.0,
+                      color: Colors.grey,
+                    )
+                  ],
+                );
+              },
+              itemCount: _items.length,
+            ),
+          ),
+        ],
       ),
       drawer: AppDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
