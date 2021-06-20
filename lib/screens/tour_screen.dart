467a1e6ea9212efb3e6cc028238cc50ed704a059
FelixSchmidt159 <flx159@yahoo.at>
2021-06-20 17:11:20 +0200

    update

--- lib/screens/tour_screen.dart
+++ lib/screens/tour_screen.dart
@@ -799,34 +799,12 @@ class _TourScreenState extends State<TourScreen> {
                   ),
                   Container(
                     padding: const EdgeInsets.all(10.0),
-                    decoration: _missingVehicle
-                        ? BoxDecoration(
-                            border: Border(
-                              top: BorderSide(
-                                width: 3.0,
-                                color: Colors.red,
-                              ),
-                              bottom: BorderSide(
-                                width: 3.0,
-                                color: Colors.red,
-                              ),
-                              left: BorderSide(
-                                width: 3.0,
-                                color: Colors.red,
-                              ),
-                              right: BorderSide(
-                                width: 3.0,
-                                color: Colors.red,
-                              ),
-                            ),
-                            borderRadius: BorderRadius.circular(10.0),
-                          )
-                        : BoxDecoration(
-                            border: Border.all(
-                              color: Colors.grey.shade400,
-                            ),
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey.shade400,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                     child: Column(
                       children: [
                         Text(
@@ -890,13 +868,34 @@ class _TourScreenState extends State<TourScreen> {
                   ),
                   Container(
                     padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color:
-                            _missingVehicle ? Colors.red : Colors.grey.shade400,
-                      ),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                    decoration: _missingVehicle
+                        ? BoxDecoration(
+                            border: Border(
+                              top: BorderSide(
+                                width: 3.0,
+                                color: Colors.red,
+                              ),
+                              bottom: BorderSide(
+                                width: 3.0,
+                                color: Colors.red,
+                              ),
+                              left: BorderSide(
+                                width: 3.0,
+                                color: Colors.red,
+                              ),
+                              right: BorderSide(
+                                width: 3.0,
+                                color: Colors.red,
+                              ),
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          )
+                        : BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey.shade400,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                     child: Column(
                       children: [
                         Text(
